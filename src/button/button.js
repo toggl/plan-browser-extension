f7290adef27f7155fd3a7624678d8516f2436655
Michal Miškerník <michal@miskernik.sk>
2015-05-11 16:35:00 +0100

    Render element with shadow DOM

--- src/button/button.js
+++ src/button/button.js
@@ -1,5 +1,19 @@
+var templates = {
+  public: require('./button_public.dom'),
+  shadow: require('./button_shadow.dom'),
+  style: require('./button.css')
+};
+
 exports.render = function() {
-  var node = document.createElement('div');
-  node.classList.add('tw-button');
-  return node;
+  var publicEl = templates.public();
+  var shadowRoot = publicEl.createShadowRoot();
+
+  var shadowEl = templates.shadow();
+  shadowRoot.appendChild(shadowEl);
+
+  var styleEl = document.createElement('style');
+  styleEl.appendChild(document.createTextNode(templates.style));
+  shadowRoot.appendChild(styleEl);
+
+  return publicEl;
 };
