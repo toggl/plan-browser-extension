1995a9c133766f942ac2372486e88c17c1258f86
Michal Miškerník <michal@miskernik.sk>
2015-05-18 08:52:36 +0100

    Added retrieving values from task view

--- src/button/task_view/task_view.js
+++ src/button/task_view/task_view.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var View = require('ampersand-view');
 var api = require('../../api/api');
 var UserListView = require('./user_list_view');
@@ -9,6 +10,10 @@ var TaskView = View.extend({
 
   collections: {
     accounts: AccountCollection
+  },
+
+  events: {
+    'click [data-hook=button-submit]': 'onSubmit'
   },
 
   render: function() {
@@ -28,6 +33,32 @@ var TaskView = View.extend({
       });
 
     return this;
+  },
+
+  onSubmit: function(event) {
+    event.preventDefault();
+
+    var values = this.getValues();
+    console.log(values);
+  },
+
+  getValues: function() {
+    var name = this.queryByHook('input-name').value;
+    var selection = JSON.parse(this.queryByHook('select-user').value);
+    var startDate = moment(this.queryByHook('input-start-date').value);
+    var endDate = moment(this.queryByHook('input-end-date').value);
+    var startTime = moment(this.queryByHook('input-start-time').value, 'HH:mm:ss');
+    var endTime = moment(this.queryByHook('input-end-time').value, 'HH:mm:ss');
+
+    return {
+      name: name,
+      account_id: selection.account,
+      user_id: selection.user,
+      start_date: startDate.isValid() ? startDate.toDate() : null,
+      end_date: endDate.isValid() ? endDate.toDate() : null,
+      start_time: startTime.isValid() ? startTime.format('HH:mm') : null,
+      end_time: endTime.isValid() ? endTime.format('HH:mm') : null
+    };
   }
 
 });
--- src/button/task_view/user_list_view.js
+++ src/button/task_view/user_list_view.js
@@ -1,4 +1,9 @@
 var View = require('ampersand-view');
+var Handlebars = require('hbsfy/runtime');
+
+Handlebars.registerHelper('user_list_value', function(options) {
+  return JSON.stringify(options.hash);
+});
 
 var UserListView = View.extend({
 
