119132d55d417f5885609a47f39b8eb85f0e2c10
Michal Miškerník <michal@miskernik.sk>
2015-05-25 20:26:43 +0200

    Added error messages

--- src/button/views/error/error_view.js
+++ src/button/views/error/error_view.js
@@ -6,15 +6,49 @@ var ErrorView = View.extend({
 
   props: {
     hub: 'state',
-    error: 'any'
+    error: 'object'
   },
 
   events: {
     'click [data-hook=button-dismiss]': 'onDismiss'
   },
 
+  messages: {
+    unknown_error: {
+      message: 'Ouch, something went wrong.',
+      contact_us: true
+    },
+    network_error: {
+      message: 'Sorry, it looks like something is wrong with the connection.',
+      contact_us: false,
+    },
+    authentication_failed: {
+      message: 'Sorry, authentication has failed.',
+      contact_us: true,
+    },
+    invalid_credentials: {
+      message: 'Sorry, the credentials you have provided are not valid.',
+      contact_us: false
+    },
+    refresh_denied: {
+      message: 'Ouch, something went wrong. Please try logging in again.',
+      contact_us: false
+    },
+    default: {
+      message: 'Something went horribly wrong, you should not even see this message',
+      contact_us: true
+    }
+  },
+
   render: function() {
-    this.renderWithTemplate(this.error);
+    var code = this.error.message;
+
+    var data = this.messages[code] != null ?
+      this.messages[code] :
+      this.messages.default;
+
+    this.renderWithTemplate(data);
+
     return this;
   },
 
