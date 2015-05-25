6db5c84a1fbcb047413cf92f8edc6f7a0b09c18f
Michal Miškerník <michal@miskernik.sk>
2015-05-25 12:47:19 +0200

    Fixed trello integration

--- src/bootloader/trello.js
+++ src/bootloader/trello.js
@@ -1,5 +1,6 @@
-var Button = require('../button/button.js');
-var btn = Button.initialize();
+var offset = require('document-offset');
+var ButtonState = require('../button/button.js');
+var btn = ButtonState.initialize();
 
 function onMutation(mutations) {
   mutations.forEach(function(mutation) {
@@ -11,14 +12,28 @@ function onMutation(mutations) {
 
 function createButton(node) {
   var titleEl = node.querySelector('.window-title');
+  var currentListEl = node.querySelector('.js-current-list');
+  var overlayEl = document.querySelector('.window-overlay');
+
   var name = titleEl.querySelector('.window-title-text').innerText;
 
-  var buttonEl = Button.create({
-    name: name
+  var state = new ButtonState({
+    task: { name: name }
   });
 
-  var currentListEl = node.querySelector('.js-current-list');
+  var buttonEl = state.button.render().el;
   titleEl.insertBefore(buttonEl, currentListEl.nextSibling);
+
+  state.on('popup:created', function() {
+    var popupEl = state.popup.render().el;
+    var buttonOffset = offset(buttonEl);
+
+    popupEl.style.position = 'absolute';
+    popupEl.style.left = (buttonOffset.left) + 'px';
+    popupEl.style.top = (buttonOffset.top + overlayEl.scrollTop) + 'px';
+
+    overlayEl.appendChild(popupEl);
+  });
 }
 
 var wrapper = document.querySelector('.window-wrapper');
