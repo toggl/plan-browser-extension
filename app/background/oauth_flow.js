c4d09e4b574bc3586fb455acf5ea3c63f2ba123d
Michal Miškerník <michal@miskernik.sk>
2015-05-27 11:38:04 +0200

    Removed background oauth script

--- app/background/oauth_flow.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(function(request, sender, respond) {
-  if (request.type === 'oauth_flow') {
-    console.log('Launching OAuth flow:', request.url);
-    chrome.identity.launchWebAuthFlow({ url: request.url, interactive: true }, respond);
-    return true;
-  }
-});
