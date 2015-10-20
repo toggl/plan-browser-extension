d8f91a02f8b7d2ff11ee2dba41fe2c0b7f7bd239
Michal Miškerník <michal@miskernik.sk>
2015-10-20 16:00:55 +0100

    Updated bootloaders

--- src/bootloader/github_issues.js
+++ src/bootloader/github_issues.js
@@ -44,7 +44,9 @@ function removeButton(node) {
 function handleError(error) {
   console.error(error);
 }
- 
-ButtonState.initialize()
-  .then(createObserver)
-  .catch(handleError);
+
+if (!ButtonState.isLoaded()) {
+  ButtonState.initialize()
+    .then(createObserver)
+    .catch(handleError);
+}
--- src/bootloader/jira.js
+++ src/bootloader/jira.js
@@ -79,6 +79,8 @@ function handleError(error) {
   console.error(error);
 }
 
-ButtonState.initialize()
-  .then(createObserver)
-  .catch(handleError);
+if (!ButtonState.isLoaded()) {
+  ButtonState.initialize()
+    .then(createObserver)
+    .catch(handleError);
+}
