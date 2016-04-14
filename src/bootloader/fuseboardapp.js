5e1e71d56847d0c0740870d607e612f08d12ba83
Enmanuel Toribio <enmanuel@torib.io>
2016-04-13 21:44:59 -0400

    Removed unnecesary code

--- src/bootloader/fuseboardapp.js
+++ src/bootloader/fuseboardapp.js
@@ -1,24 +1,7 @@
 'use strict';
 const twb = require('../utils/content');
 
-twb.observe('ul#tasklists ul.tasks li, #deal-list tr .summary, #ticket-list tr .summary', (bubble) => {
-  if (!bubble) return;
-  let titleElement = bubble.querySelector("a");
-  if(!titleElement) return;
-  let title = titleElement.textContent.trim();
-  let button = twb.create({
-    task: {name: title},
-    anchor: 'screen'
-  });
-  let container = bubble.querySelector('h6');
-  twb.prepend(button, container);
-  return button;
-
-}, (button) => {
-  twb.remove(button);
-});
-
-twb.observe('#project-list tr', (bubble) => {
+twb.observe('ul#tasklists ul.tasks li, #deal-list tr .summary, #ticket-list tr .summary, #project-list tr', (bubble) => {
   if (!bubble) return;
   let titleElement = bubble.querySelector("a");
   if(!titleElement) return;
