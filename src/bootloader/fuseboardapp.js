5f4b6f30cf8ac0121c5355bb35cf405e245d82c7
Michal Miškerník <michal@miskernik.sk>
2016-10-31 08:44:37 +0000

    Fixed Fuseboard integration

--- src/bootloader/fuseboardapp.js
+++ src/bootloader/fuseboardapp.js
@@ -1,34 +1,53 @@
 'use strict';
 const twb = require('../utils/content');
 
-twb.observe('ul#tasklists ul.tasks li, #deal-list tr .summary, #ticket-list tr .summary, #project-list tr', (bubble) => {
-  if (!bubble) return;
-  let titleElement = bubble.querySelector("a");
-  if(!titleElement) return;
-  let title = titleElement.textContent.trim();
+/* Projects/Deals/Tickets list items */
+/* Project/Deal/Ticket task list items */
+twb.observe('ul#tasklists ul.tasks li, #deal-list tr .summary, #ticket-list tr .summary, #project-list tr', (itemEl) => {
+  if (!itemEl) return;
+
+  let titleEl = itemEl.querySelector("a");
+  if (!titleEl) return;
+
+  let title = titleEl.textContent.trim();
+
   let button = twb.create({
     task: {name: title},
     anchor: 'screen'
   });
-  let container = bubble.querySelector('h6');
-  twb.prepend(button, container);
+
+  let containerEl = itemEl.querySelector('h6');
+  if (!containerEl) return;
+
+  twb.prepend(button, containerEl);
+
   return button;
 }, (button) => {
+  if (!button) return;
   twb.remove(button);
 });
 
-twb.observe('ul.deals li, ul.projects li, ul.tickets li', (bubble) => {
-  if (!bubble) return;
-  let titleElement = bubble.querySelector(".board-card-title");
-  if(!titleElement) return;
-  let title = titleElement.textContent.trim();
+/* Projects/Deals/Tickets board cards */
+twb.observe('ul.deals li, ul.projects li, ul.tickets li', (itemEl) => {
+  if (!itemEl) return;
+
+  let titleEl = itemEl.querySelector(".board-card-title");
+  if (!titleEl) return;
+
+  let title = titleEl.textContent.trim();
+
   let button = twb.create({
     task: {name: title},
     anchor: 'screen'
   });
-  let container = bubble.querySelector('.board-snapshot-icons');
-  twb.append(button, container);
+
+  let containerEl = itemEl.querySelector('.board-snapshot-icons');
+  if (!containerEl) return;
+
+  twb.append(button, containerEl);
+
   return button;
 }, (button) => {
+  if (!button) return;
   twb.remove(button);
 });
