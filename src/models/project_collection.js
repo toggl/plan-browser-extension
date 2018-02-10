4e6d223dbbc1756c43dc76f43d27b6b1b8766bf9
Mitchel Kelonye <kelonyemitchel@gmail.com>
2018-02-10 13:03:15 +0300

    Update api endpoint
    Ref Teamweek/teamweek#2510

--- src/models/project_collection.js
+++ src/models/project_collection.js
@@ -7,7 +7,7 @@ const ProjectCollection = RestCollection.extend({
   model: ProjectModel,
 
   url() {
-    return config.api.host + '/api/v3/' + this.parent.id + '/projects';
+    return config.api.host + '/timeline/v1/' + this.parent.id + '/projects';
   },
 
   sync
