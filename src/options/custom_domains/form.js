2de0abeeb4a582647a7424baebe7d9ac1666978a
Mitchel Kelonye <kelonyemitchel@gmail.com>
2018-04-06 11:07:13 +0300

    Fix issue where some domains were being cut off
    Closes Teamweek/tw-button#34

--- src/options/custom_domains/form.js
+++ src/options/custom_domains/form.js
@@ -2,8 +2,6 @@ const View = require('ampersand-view');
 const permissions = require('../../utils/permissions');
 const services = require('./services.json');
 
-const DOMAIN_RE = /((?:\w+\.)*\w+)/;
-
 const FormView = View.extend({
 
   template: require('./form.hbs'),
@@ -22,22 +20,13 @@ const FormView = View.extend({
   },
 
   onCreate() {
-    let domain = this.queryByHook('domain').value;
+    const domain = this.queryByHook('domain').value;
     const service = this.queryByHook('service').value;
 
     if (Object.keys(services).indexOf(service) < 0) {
       this.error = 'Please select a service';
       return;
     }
-
-    const matches = DOMAIN_RE.exec(domain);
-
-    if (!matches) {
-      this.error = 'Please enter a valid domain, e.g. mydomain.com';
-      return;
-    }
-
-    domain = matches[1];
 
     permissions.request(['tabs', 'webNavigation'], domain)
       .then(() => this.collection.create({
