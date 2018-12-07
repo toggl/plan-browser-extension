21efa992b20002a297a1a802c8a46292125d7a2f
Ignacio Picun <ignaciopicun@protonmail.com>
2018-12-07 01:22:16 -0300

    updated api token url from v3 to v4

--- src/models/authentication_state.js
+++ src/models/authentication_state.js
@@ -8,14 +8,14 @@ const TokensModel = require('./tokens_model');
 const AuthenticationState = State.extend({
   props: {
     // Property for storing the promise during a token refresh
-    refresh_promise: 'object'
+    refresh_promise: 'object',
   },
 
   children: {
     // State for storing client ID and secret
     oauth: OAuthState,
     // Model for storing access and refresh tokens
-    tokens: TokensModel
+    tokens: TokensModel,
   },
 
   derived: {
@@ -24,15 +24,16 @@ const AuthenticationState = State.extend({
       deps: ['tokens.has_auth_tokens'],
       fn() {
         return this.tokens.has_auth_tokens;
-      }
-    }
+      },
+    },
   },
 
   initialize() {
     // Initialize client ID and secret
     this.oauth.set({
-      id: '9c782f95e771811bdedb77dc12e6be98ca286ea8',
-      secret: '441a5bc3e3828910866ea5929fc0313e63a71a77934f3e248a4322f830253c2164e4cd33bead4e3397a27e2b9863ba4a'
+      id: '2cf9a67253a2dbcd3ce61a334c9634fae9ad750e',
+      secret:
+        '441a5bc3e3828910866ea5929fc0313e63a71a77934f3e248a4322f830253c2164e4cd33bead4e3397a27e2b9863ba4a',
     });
   },
 
@@ -75,14 +76,15 @@ const AuthenticationState = State.extend({
     return new Promise((resolve, reject) => {
       // Create a request that will return access and refresh tokens
       request
-        .post(config.api.host + '/api/v3/authenticate/token')
+        .post(config.api.host + '/api/v4/authenticate/token')
         // Use base64'd client ID and secret for authorization
         .set('Authorization', 'Basic ' + this.oauth.token)
         // Send credentials in form data
-        .type('form').send({
+        .type('form')
+        .send({
           grant_type: 'password',
           username: credentials.username,
-          password: credentials.password
+          password: credentials.password,
         })
         // Send the request
         .end((error, response) => {
@@ -90,15 +92,15 @@ const AuthenticationState = State.extend({
           if (!response) {
             reject({ message: 'network_error' });
 
-          // If everything is fine, we save the tokens to local storage
+            // If everything is fine, we save the tokens to local storage
           } else if (response.ok) {
             resolve(this.tokens.save(response.body));
 
-          // If the credentials are invalid, return an error
+            // If the credentials are invalid, return an error
           } else if (response.clientError) {
             reject({ message: 'invalid_credentials' });
 
-          // If something weird happens, return an error
+            // If something weird happens, return an error
           } else {
             reject({ message: 'unknown_error' });
           }
@@ -117,13 +119,14 @@ const AuthenticationState = State.extend({
       this.refresh_promise = new Promise((resolve, reject) => {
         // Create a request that will fetch new tokens
         request
-          .post(config.api.host + '/api/v3/authenticate/token')
+          .post(config.api.host + '/api/v4/authenticate/token')
           // Use base64'd client ID and secret for authorization
           .set('Authorization', 'Basic ' + this.oauth.token)
           // Send refresh token in form data
-          .type('form').send({
+          .type('form')
+          .send({
             refresh_token: this.tokens.refresh_token,
-            grant_type: 'refresh_token'
+            grant_type: 'refresh_token',
           })
           .end((error, response) => {
             // Refresh is finished, another can start
@@ -133,24 +136,27 @@ const AuthenticationState = State.extend({
             if (!response) {
               reject({ message: 'network_error' });
 
-            // If everything is fine, we save the tokens to local storage
+              // If everything is fine, we save the tokens to local storage
             } else if (response.ok) {
               const result = this.tokens.set(response.body).save();
               resolve(result);
-            // If the refresh token is invalid, we clear tokens, remove them
-            // from local storage and return an error
+              // If the refresh token is invalid, we clear tokens, remove them
+              // from local storage and return an error
             } else if (response.clientError) {
               // tokens.destroy() will return a resolved promise, so we need to
               // reject it with the refresh denied error
-              const result = this.tokens.clear().destroy().then(() => {
-                return Promise.reject({ message: 'refresh_denied' });
-              });
+              const result = this.tokens
+                .clear()
+                .destroy()
+                .then(() => {
+                  return Promise.reject({ message: 'refresh_denied' });
+                });
 
               // if the result promise is rejected,
               // this promise will be rejected too
               resolve(result);
 
-            // If something weird happens, return an error
+              // If something weird happens, return an error
             } else {
               reject({ message: 'unknown_error' });
             }
@@ -160,8 +166,7 @@ const AuthenticationState = State.extend({
 
     // Return a new or existing refresh promise
     return this.refresh_promise;
-  }
-
+  },
 });
 
 module.exports = AuthenticationState;
