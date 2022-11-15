6747faf66d1ca2e58125057fbac87c893f8b7284
prssanna <prssud@gmail.com>
2022-11-15 17:52:36 +0530

    fix: don't open new window every time extension button is clicked

--- src/background/browser_action.js
+++ src/background/browser_action.js
@@ -1,15 +1,35 @@
 import config from 'src/api/config';
-import { addOnExtensionClickListener, createURL } from './util';
+import {
+  addExtensionRemovedListener,
+  addOnExtensionClickListener,
+  createURL,
+} from './util';
 
-addOnExtensionClickListener(function() {
-  chrome.windows.getCurrent({}, win => {
-    chrome.windows.create({
-      url: createURL({ name: '' }),
-      width: config.popup.width,
-      height: config.popup.height,
-      top: 120,
-      left: win.width - config.popup.width - 20,
-      type: 'popup',
-    });
-  });
+let windowId = false;
+
+addOnExtensionClickListener(win => {
+  if (windowId === false) {
+    windowId = true;
+    chrome.windows.create(
+      {
+        url: createURL({ name: '' }),
+        width: config.popup.width,
+        height: config.popup.height,
+        top: 120,
+        left: win.width - config.popup.width - 20,
+        type: 'popup',
+      },
+      win => {
+        windowId = win.id;
+      }
+    );
+  } else if (typeof windowId === 'number') {
+    chrome.windows.update(windowId, { focused: true });
+  }
+});
+
+addExtensionRemovedListener(winId => {
+  if (windowId === winId) {
+    windowId = false;
+  }
 });
